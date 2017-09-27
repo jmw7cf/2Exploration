8aeacac8bfa88d70dd89be939c0a69c7e9e0baa1
Julia Wopata <julia.wopata@gmail.com>
2017-09-27 16:39:27 -0500

    fixes the error for checkURl not found and adds the li back in

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 var default_content="";
-
-$(document).ready(function () {
-   
-    checkURL(); //this will check if the URL has a reference to a page. It will load it.
-    
-    $('ul li a').click(function(e){ //go through all of our navigation links
-        checkURL(this.hash); // assigns them to an onclick event using their own hash
-    });
-    
-    //filling in the default content
-	default_content = $('#pageContent').html();
-    
-    setInterval("checkUrl()", 250); //checks for a change in the URL every ms. This is to see if history buttons (back) have been used.
-});
-
 var lasturl=""; //stores the current URL hash
 
 function checkURL(hash){
@@ -29,10 +14,27 @@ function checkURL(hash){
 		else
 		loadPage(hash);
     }
-}
+};
+
+$(document).ready(function () {
+   
+    checkURL(); //this will check if the URL has a reference to a page. It will load it.
+    
+    $('ul li a').click(function(e){ //go through all of our navigation links
+        checkURL(this.hash); // assigns them to an onclick event using their own hash
+    });
+    
+    //filling in the default content
+	default_content = $('#pageContent').html();
+    
+    setInterval(checkUrl, 250); //checks for a change in the URL every ms. This is to see if history buttons (back) have been used.
+});
+
+
 
 $('.carousel[data-type="multi"] .item').each(function(){
   var next = $(this).next();
+    console.log(next);
   if (!next.length) {
     next = $(this).siblings(':first');
   }
@@ -49,39 +51,7 @@ $('.carousel[data-type="multi"] .item').each(function(){
 });
 
 function loadPage(url) { //loads the page via AJAx
-    
-       url=url.replace('#', '');
-//    function stageContent(content){
-//                $("#pageContent").html(content);
-//            }
-//            
-//            function evaluateHash() {
-//                if(location.hash == "#JohnMayer") {
-//                    $.get("about.html", stageContent);
-//                }
-//                else if(location.hash == "#TaylorSwift") {
-//                    $.get("TaylorSwift.html", pageContent);
-//                }
-//                else if(location.hash == "#RyanReynolds") {
-//                    $.get("RyanReynolds.html", pageContent);
-//                }
-//                else if(location.hash == "#JimmyFallon") {
-//                    $.get("JimmyFallon.html", pageContent);
-//                }
-//                else if(location.hash == "#NationalParkService") {
-//                    $.get("NationalParkService.html", pageContent);
-//                }
-//                else {
-//                    $.get("index.html", pageContent);
-//                }
-//            }
-//            
-//            $(function(){
-//                evaluateHash();
-//                window.onhashchange = evaluateHash;
-//            });
-//    
-
+    url=url.replace('#', '');
     $.ajax({
         type: "POST",
         url: "load_page.php",
