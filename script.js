f1a3840b8df1ebe768ca89981029c61f58011ced
Julia Wopata <julia.wopata@gmail.com>
2017-09-26 22:49:22 -0500

    Added images and script/css/html for carousel

--- script.js
+++ script.js
@@ -31,6 +31,23 @@ function checkURL(hash){
     }
 }
 
+$('.carousel[data-type="multi"] .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  for (var i=0;i<2;i++) {
+    next=next.next();
+    if (!next.length) {
+    	next = $(this).siblings(':first');
+  	}
+    
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
+
 function loadPage(url) { //loads the page via AJAx
     
        url=url.replace('#', '');
